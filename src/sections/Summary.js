6c3b16ea09b8f26eb38cfb08c7b3f684b7321e36
Henry Larson <hxl1116@rit.edu>
2021-07-03 23:10:50 -0400

    Stubbed out info summary table pagination; TODOs marked

--- src/sections/Summary.js
+++ src/sections/Summary.js
@@ -1,15 +1,19 @@
-import React from "react";
+import React, {useEffect} from "react";
 import {
     Paper,
     Table,
     TableBody,
     TableCell,
     TableContainer,
-    TableHead,
+    TableHead, TablePagination,
     TableRow
 } from "@material-ui/core";
 
 export default function Summary(props) {
+    useEffect(() => {
+        console.log(props.data)
+    }, [])
+
     const handleValue = (value) => {
         switch (typeof value) {
             case "boolean":
@@ -22,33 +26,37 @@ export default function Summary(props) {
     }
 
     return (
-        <TableContainer component={Paper}>
-            <Table aria-label={'summary-table'}>
-                <TableHead>
-                    <TableRow>
-                        <TableCell align="left">
-                            <p>Field description</p>
-                        </TableCell>
-                        <TableCell align="right">
-                            <p>Field information</p>
-                        </TableCell>
-                    </TableRow>
-                </TableHead>
-                <TableBody>
-                    {Object.values(props.data).map(section => (
-                        Object.values(section).map(field => (
-                            <TableRow>
-                                <TableCell align="left">
-                                    <p>{field.label}</p>
-                                </TableCell>
-                                <TableCell align="right">
-                                    <p>{handleValue(field.value)}</p>
-                                </TableCell>
-                            </TableRow>
-                        ))
-                    ))}
-                </TableBody>
-            </Table>
-        </TableContainer>
+        <>
+            <TableContainer component={Paper}>
+                <Table size="small" aria-label={'summary-table'}>
+                    <TableHead>
+                        <TableRow>
+                            <TableCell align="left">
+                                <p>Field description</p>
+                            </TableCell>
+                            <TableCell align="right">
+                                <p>Field information</p>
+                            </TableCell>
+                        </TableRow>
+                    </TableHead>
+                    <TableBody>
+                        {Object.values(props.data).map(section => (
+                            Object.values(section).map(field => (
+                                <TableRow>
+                                    <TableCell align="left">
+                                        <p>{field.label}</p>
+                                    </TableCell>
+                                    <TableCell align="right">
+                                        <p>{handleValue(field.value)}</p>
+                                    </TableCell>
+                                </TableRow>
+                            ))
+                        ))}
+                    </TableBody>
+                </Table>
+            </TableContainer>
+            {/* TODO: Implement pagination */}
+            <TablePagination count={props.data.length} onChangePage={() => {}} page={0} rowsPerPage={1}/>
+        </>
     )
 }
