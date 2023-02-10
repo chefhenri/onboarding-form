3e64def1541f0b63f0833124f7a52be886599299
Henry Larson <henryclarson22@gmail.com>
2023-02-09 23:01:06 -0500

    TODOs marked

--- src/components/panels/InfoPanel.jsx
+++ src/components/panels/InfoPanel.jsx
@@ -3,6 +3,7 @@ import { Paper, Typography } from "@mui/material"
 const InfoPanel = ({ info }) => {
     return (
         // TODO: Add ::before element to fade overflow text
+        // TODO: Widen surface to make more room for info text
         <Paper elevation={3} sx={{ width: '200px', height: '270px', padding: '1rem', borderRadius: '15px', overflowY: 'auto' }}>
             <Typography>{info}</Typography>
         </Paper>
