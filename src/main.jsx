ff92818d028e096d426a9a2372458985bbd76c9b
Henry Larson <henryclarson22@gmail.com>
2023-02-10 21:53:07 -0500

    Revert "Form Generation from File"

--- src/main.jsx
+++ src/main.jsx
@@ -1,9 +1,6 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 
-import fs from 'fs'
-import YAML from 'yaml'
-
 import App from './App'
 
 ReactDOM.createRoot(document.querySelector('#root')).render(
--- vite.config.js
+++ vite.config.js
@@ -3,10 +3,5 @@ import react from '@vitejs/plugin-react-swc'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  server: {
-    fs: {
-      allow: ['..']
-    }
-  },
   plugins: [react()],
 })
