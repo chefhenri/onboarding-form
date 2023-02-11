8954ad9ee10bdcfab8dc938e55ebc3eef7e5a330
Henry Larson <henryclarson22@gmail.com>
2023-02-10 21:29:06 -0500

    `Wrapper` loads actual form data instead of dummy data; Removed height restriction on `ContentsPanel`

--- src/main.jsx
+++ src/main.jsx
@@ -3,8 +3,10 @@ import ReactDOM from 'react-dom/client'
 
 import App from './App'
 
+import { sections } from './assets/onboarding-form.json'
+
 ReactDOM.createRoot(document.querySelector('#root')).render(
   <React.StrictMode>
-    <App />
+    <App {...{ sections }} />
   </React.StrictMode>,
 )
