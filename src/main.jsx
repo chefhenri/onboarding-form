5ab5154358ed50a353554e130c92949f9aab3cc7
Henry Larson <henryclarson22@gmail.com>
2023-02-10 20:07:08 -0500

    Added `yaml` dep

--- src/main.jsx
+++ src/main.jsx
@@ -1,6 +1,9 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 
+import fs from 'fs'
+import YAML from 'yaml'
+
 import App from './App'
 
 ReactDOM.createRoot(document.querySelector('#root')).render(
