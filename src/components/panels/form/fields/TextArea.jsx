e324afaf8f78b052b5bc2d744f6a6714082a48ab
Henry Larson <henryclarson22@gmail.com>
2023-02-19 15:44:58 -0500

    Added controlled state to text field and text area

--- src/components/panels/form/fields/TextArea.jsx
+++ src/components/panels/form/fields/TextArea.jsx
@@ -1,9 +1,16 @@
+import { useState } from "react";
 import { Grid, TextField } from "@mui/material";
 
 const FormTextArea = ({ id, name, label, required }) => {
+    const [value, setValue] = useState('')
+
+    const handleChange = (event) => {
+        setValue(event.target.value)
+    }
+
     return (
         <Grid item xs={12}>
-            <TextField fullWidth multiline variant="filled" rows={5} {...{id, name, label, required}} />
+            <TextField fullWidth multiline variant="filled" rows={5} {...{id, name, label, required, value}} onChange={event => handleChange(event)} />
         </Grid>
     )
 }
--- src/components/panels/form/fields/TextField.jsx
+++ src/components/panels/form/fields/TextField.jsx
@@ -1,9 +1,16 @@
+import { useState } from "react";
 import { Grid, TextField } from "@mui/material";
 
 const FormTextField = ({ id, name, type, label, required }) => {
+    const [value, setValue] = useState('')
+
+    const handleChange = (event) => {
+        setValue(event.target.value)
+    }
+
     return (
         <Grid item xs={6}>
-            <TextField fullWidth variant="filled" {...{ id, name, type, label, required }} />
+            <TextField fullWidth variant="filled" {...{ id, name, type, label, required, value }} onChange={event => handleChange(event)} />
         </Grid>
     )
 }
