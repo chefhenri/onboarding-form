ea66aa768bba9728d2bcc9267a7dabe6a80cf1ff
Henry Larson <henryclarson22@gmail.com>
2023-02-19 22:28:41 -0500

    Moved `LocalizationProvider` wrapper and `CssBaseline` to `main.jsx`; TODOs added

--- src/main.jsx
+++ src/main.jsx
@@ -1,12 +1,20 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 
+import { LocalizationProvider } from "@mui/x-date-pickers";
+import { AdapterMoment } from "@mui/x-date-pickers/AdapterMoment";
+import { CssBaseline } from '@mui/material';
+
 import App from './App'
 
 import { sections } from './assets/onboarding-form.json'
 
+// TODO: Add themeing
 ReactDOM.createRoot(document.querySelector('#root')).render(
   <React.StrictMode>
-    <App {...{ sections }} />
-  </React.StrictMode>,
+    <LocalizationProvider dateAdapter={AdapterMoment}>
+      <CssBaseline />
+      <App {...{ sections }} />
+    </LocalizationProvider>
+  </React.StrictMode>
 )
