6b976edaf75ee36726103a1bc4cdf8ea3abc1911
Henry Larson <henryclarson22@gmail.com>
2023-02-19 23:01:50 -0500

    Moved `LocalizationProvider`, `CssBaseline` back to `App.jsx`; Theme detects light/dark mode

--- src/main.jsx
+++ src/main.jsx
@@ -1,20 +1,12 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 
-import { LocalizationProvider } from "@mui/x-date-pickers";
-import { AdapterMoment } from "@mui/x-date-pickers/AdapterMoment";
-import { CssBaseline } from '@mui/material';
-
 import App from './App'
 
 import { sections } from './assets/onboarding-form.json'
 
-// TODO: Add themeing
 ReactDOM.createRoot(document.querySelector('#root')).render(
   <React.StrictMode>
-    <LocalizationProvider dateAdapter={AdapterMoment}>
-      <CssBaseline />
-      <App {...{ sections }} />
-    </LocalizationProvider>
+    <App {...{ sections }} />
   </React.StrictMode>
 )
