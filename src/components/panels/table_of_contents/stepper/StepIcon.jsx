7169e210c47fa31d682945d349222f12b412b193
Henry Larson <henryclarson22@gmail.com>
2023-02-20 21:56:59 -0500

    Added custom stepper icon to table of contents panel

--- src/components/panels/table_of_contents/stepper/StepIcon.jsx
+++ src/components/panels/table_of_contents/stepper/StepIcon.jsx
@@ -2,52 +2,29 @@ import { styled } from "@mui/material";
 
 const StyledIconRoot = styled('div')(({ theme, ownerState }) => ({
     display: 'flex',
-    width: 22,
-    height: 22,
     alignItems: 'center',
     ...(ownerState.active && {
         color: 'palette.primary.main'
     }),
     '& .StyledStepIcon-circle': {
-        width: 8,
-        height: 8,
-        borderRadius: '50%',
-        borderStyle: 'solid',
-        borderWidth: 1,
-        borderColor: '#0066FF',
-        transitionDuration: '300ms',
-        transitionProperty: 'background-color',
-        transitionTimingFunction: 'ease-in-out'
+        display: 'none'
     },
     '& .StyledStepIcon-active': {
-        width: 8,
-        height: 8,
-        borderRadius: '50%',
-        borderStyle: 'solid',
-        borderWidth: 1,
-        borderColor: '#0066FF',
-        backgroundColor: '#0066FF',
-        transitionDuration: '300ms',
-        transitionProperty: 'background-color',
-        transitionTimingFunction: 'ease-in-out'
+        display: 'block',
+        width: 4,
+        height: 22,
+        borderTopRightRadius: 15,
+        borderBottomRightRadius: 15,
+        backgroundColor: '#0066FF'
     },
     '& .StyledStepIcon-completed': {
-        width: 8,
-        height: 8,
-        borderRadius: '50%',
-        borderStyle: 'solid',
-        borderWidth: 1,
-        borderColor: '#0066FF',
-        backgroundColor: '#0066FF',
-        transitionDuration: '300ms',
-        transitionProperty: 'background-color',
-        transitionTimingFunction: 'ease-in-out'
+        display: 'none'
     },
 }))
 
 const StyledStepIcon = ({ active, completed, className }) => {
     return (
-        <StyledIconRoot {...{ ownerState: active, className }} sx={{borderWidth: '1px', borderColor: '#0066FF'}}>
+        <StyledIconRoot {...{ ownerState: active, className }} sx={{ borderWidth: '1px', borderColor: '#0066FF' }}>
             {completed ? (
                 <div className="StyledStepIcon-completed" />
             ) : (
