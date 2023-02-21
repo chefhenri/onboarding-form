76993552e729a59940b7459c0c9ab93bcf14c544
Henry Larson <henryclarson22@gmail.com>
2023-02-21 11:12:36 -0500

    Added redux toolkit, react-redux deps; wrapped `App` with store provider

--- src/main.jsx
+++ src/main.jsx
@@ -1,12 +1,16 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
+import { Provider } from 'react-redux'
 
 import App from './App'
+import store from './store'
 
 import { sections } from './assets/onboarding-form.json'
 
 ReactDOM.createRoot(document.querySelector('#root')).render(
   <React.StrictMode>
-    <App {...{ sections }} />
+    <Provider {...{ store }}>
+      <App {...{ sections }} />
+    </Provider>
   </React.StrictMode>
 )
