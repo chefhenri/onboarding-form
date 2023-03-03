824bd402b71190f3ef538275c067ace92e727353
Henry Larson <henryclarson22@gmail.com>
2023-03-03 16:33:11 -0500

    extracted review panel section list item to `ReviewItem` component

--- src/components/panels/review/Item.jsx
+++ src/components/panels/review/Item.jsx
@@ -1,21 +1,13 @@
 import _ from 'lodash'
 import moment from 'moment'
-import { useSelector } from "react-redux"
-import { Fragment, useState } from 'react'
-import { Divider, ListItem, Stack, Typography } from "@mui/material"
-import ReviewHeading from './Heading'
+import { useSelector } from 'react-redux'
+import { Divider, Stack, Typography } from "@mui/material"
 
 const DATE_FMT = 'MM/DD/YYYY'
 
-// TODO: Add handle for review heading 'edit' button
-const ReviewItem = ({ heading, subsections }) => {
+const ReviewItem = ({ id, name, type, label, options }) => {
     const formData = useSelector((state) => state.form)
-
-    const [showEdit, setShowEdit] = useState(false)
-
-    const toggleShowEdit = () => {
-        setShowEdit(!showEdit)
-    }
+    const fmtAsGroup = type === 'checkbox'
 
     const formatValue = (name, type, options) => {
         let value = formData[name]
@@ -23,8 +15,8 @@ const ReviewItem = ({ heading, subsections }) => {
         if (value === undefined) return
 
         switch (type) {
-            case 'checkbox':
-                return value ? 'Yes' : 'No'
+            // case 'checkbox':
+            //     return value ? 'Yes' : 'No'
             case 'date':
                 return moment(value).format(DATE_FMT)
             case 'select':
@@ -39,43 +31,53 @@ const ReviewItem = ({ heading, subsections }) => {
     }
 
     return (
-        <ListItem onMouseEnter={toggleShowEdit} onMouseLeave={toggleShowEdit}>
-            <Stack direction="column" width="100%" spacing={1}>
-                <ReviewHeading {...{ heading, show: showEdit }} />
-                {subsections.map(({ fields }) => {
-                    return fields.map(({ id, name, type, label, options }) => {
-                        let fmtGroup = type === 'checkbox'
+        fmtAsGroup ? (
+            <>
+                <Typography component="span" sx={{ width: '50%', opacity: '62%' }}>
+                    {label}
+                </Typography>
+                {options.map((opt, idx) => {
+                    let nom = _.camelCase([name, 'opt', idx])
+                    let val = formatValue(nom, type)
 
-                        return (
-                            fmtGroup ? (
-                                <Fragment key={_.kebabCase([id, 'record'])}>
-                                    <Typography>{label}</Typography>
-                                    {options.map((opt, idx) => {
-                                        let nom = _.kebabCase([name, opt, idx])
-
-                                        return (
-                                            <Stack key={_.kebabCase([nom, 'record'])} direction="row-reverse" width="100%" sx={{ justifyContent: 'space-between', paddingLeft: '1rem' }}>
-                                                <Typography>{formatValue(nom, type)}</Typography>
-                                                <Typography>{opt}</Typography>
-                                            </Stack>
-                                        )
-                                    })}
-                                    <Divider />
-                                </Fragment>
-                            ) : (
-                                <Fragment key={_.kebabCase([id, 'record'])}>
-                                    <Stack direction="row" sx={{ justifyContent: 'space-between' }}>
-                                        <Typography>{label}</Typography>
-                                        <Typography>{formatValue(name, type, options)}</Typography>
-                                    </Stack>
-                                    <Divider />
-                                </Fragment>
-                            )
-                        )
-                    })
+                    return (
+                        <Stack
+                            key={_.kebabCase([nom, 'record'])}
+                            direction="row-reverse"
+                            sx={{ width: '100%' }}>
+                            <Typography
+                                component="span"
+                                sx={{
+                                    // paddingLeft: '1rem',
+                                    width: '50%',
+                                    // width: '100%',
+                                    // textAlign: 'left',
+                                    // fontWeight: val ? 500 : 400,
+                                    opacity: val ? '100%' : '62%'
+                                }}>
+                                {opt}
+                            </Typography>
+                            {/* <Typography component="span">
+                                {formatValue(nom, type)}
+                            </Typography> */}
+                        </Stack>
+                    )
                 })}
-            </Stack>
-        </ListItem>
+                <Divider />
+            </>
+        ) : (
+            <>
+                <Stack direction="row">
+                    <Typography component="span" sx={{ width: '50%', opacity: '62%' }}>
+                        {label}
+                    </Typography>
+                    <Typography>
+                        {formatValue(name, type, options)}
+                    </Typography>
+                </Stack>
+                <Divider />
+            </>
+        )
     )
 }
 
--- src/components/panels/review/List.jsx
+++ src/components/panels/review/List.jsx
@@ -1,14 +1,14 @@
 import _ from 'lodash'
 import { Box, List } from "@mui/material"
 
-import ReviewItem from './Item'
+import ReviewSection from './Section'
 
 const ReviewList = ({ sections }) => {
     return (
-        <Box sx={{ marginY: '2rem', width: '100%', overflowY: 'scroll' }}>
+        <Box sx={{ marginY: '2rem', width: '100%', height: '100%', overflowY: 'scroll' }}>
             <List>
-                {sections.map(({ id, heading, subsections }) => (
-                    <ReviewItem key={_.kebabCase([id, 'record'])} {...{ heading, subsections }} />
+                {sections.map(({ id, heading, subsections }, idx) => (
+                    <ReviewSection key={_.kebabCase([id, 'record'])} {...{ id: idx, heading, subsections }} />
                 ))}
             </List>
         </Box>
